76223dff60531a4dced4319c824f07c7b917a2b9
smrsun4 <smrsun4@gmail.com>
2024-09-14 19:16:28 -0400

    added resume code to resume jsx

---
+++ src/pages/resume.jsx
@@ -0,0 +1,12 @@
+const Resume = () => {
+    <section id="resume">
+        <a href="path/to/resume.pdf" download>Download Resume</a>
+        <ul>
+            <li>Proficiencies</li>
+            <li>Proficiencies</li>
+            <li>Proficiencies</li>
+        </ul>
+    </section>
+};
+
+export default Resume;
