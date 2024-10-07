c5649070917578fd247d731e88dd1aa2519ef610
smrsun4 <smrsun4@gmail.com>
2024-10-07 10:00:02 -0400

    added resume, proficiencies, ul's and div classes

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,12 +1,33 @@
-const Resume = () => {
-    <section id="resume">
-        <a href="path/to/resume.pdf" download>Download Resume</a>
-        <ul>
-            <li>Proficiencies</li>
-            <li>Proficiencies</li>
-            <li>Proficiencies</li>
-        </ul>
-    </section>
-};
+import React from 'react';
+
+function Resume() {
+    return (
+        <section id="resume">
+            <h1>Resume</h1>
+            <div className="resume-content">
+                <div className="resume-download">
+                    <a href="/path-to-your-resume.pdf" download className="download-button">
+                        Download Resume
+                    </a>
+                </div>
+                <div className="skills">
+                    <h2>Proficiencies</h2>
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Node.js</li>
+                        <li>Express.js</li>
+                        <li>MongoDB</li>
+                        <li>SQL</li>
+                        <li>RESTful APIs</li>
+                        <li>Git</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
+}
 
 export default Resume;
